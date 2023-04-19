b912a571e36b0a7f0310cd80cf98291e69e1124d
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2023-04-20 00:07:43 +0600

    ant design and side bar added

---
+++ pages/dashboard.js
@@ -0,0 +1,12 @@
+import DashboardLayout from ' @/components/Layouts/DashboardLayout';
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <DashboardLayout>Hello</DashboardLayout>
+        </div>
+    );
+};
+
+export default Dashboard;
